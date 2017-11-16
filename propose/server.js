b20b34b9fce7d90616aff5b680bae03e7cada26f
alganbr <algan_br@hotmail.com>
2017-11-15 18:53:54 -0800

    Hot Reloading
    - enables hot reloading
    - run node ./server.js in different terminal to activate
    webpack-dev-server
    - any changes made on the page will automatically be displayed in the
    browser

--- propose/server.js
+++ propose/server.js
@@ -4,9 +4,14 @@ var config = require('./webpack.local.config')
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
-  hot: true,
+  public: '10.10.10.61:3000',
   inline: true,
   historyApiFallback: true,
+  watch: true,
+  watchOptions: {
+  	aggregateTimeout: 300,
+    poll: 1000
+  }
 }).listen(3000, config.ip, function (err, result) {
   if (err) {
     console.log(err)
