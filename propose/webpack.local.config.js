2281d5969807d38c1cd59efb2a6d61f1da56ac06
alganbr <algan_br@hotmail.com>
2017-11-28 18:41:20 -0800

    update react and hot-reloading version
    - react: 16.0.0
    - react-hot-reloader: 3.1.3
    - webpack: 3.3.0

--- propose/webpack.local.config.js
+++ propose/webpack.local.config.js
@@ -19,12 +19,12 @@ config.output.publicPath = 'http://' + ip + ':3000' + '/assets/bundles/'
 
 config.plugins = config.plugins.concat([
   new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin(),
+  new webpack.NoEmitOnErrorsPlugin(),
   new BundleTracker({filename: './webpack-stats-local.json'}),
 ])
 
 config.module.loaders.push(
-  { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] }
+  { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot-loader/webpack', 'babel-loader'] }
 )
 
 module.exports = config
