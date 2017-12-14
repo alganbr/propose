3f565da4d91534fcceb5a30277de490f4b0da303
alganbr <algan_br@hotmail.com>
2017-12-14 15:58:23 -0800

    task detail API
    GET /api/projects/<id>/task/<task_number>
      - get a specific task
    POST /api/projects/<id>/task/<task_number>
     - edit a specific task

--- propose/project/views.py
+++ propose/project/views.py
@@ -56,9 +56,39 @@ class ProjectTask(APIView):
 
     def post(self, request, pk, format=None):
         project = self.get_object(pk)
+        task_number = Task.objects.filter(project=project.pk).count()+1
         task_data = request.data
         task_data['project'] = project.pk
+        task_data['task_number'] = task_number
         serializer = TaskCreateSerializer(data=task_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProjectTaskDetail(APIView):
+    """
+    /api/projects/<id>/tasks/<task_number>
+    """
+
+    def get_object(self, pk):
+        return get_object_or_404(Project, pk=pk)
+
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        task_number = self.kwargs.get('task_number')
+        project = self.get_object(pk)
+        task = get_object_or_404(Task, project=project, task_number=task_number)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        task_number = self.kwargs.get('task_number')
+        project = self.get_object(pk)
+        task = get_object_or_404(Task, project=project, task_number=task_number)
+        task_data = request.data
+        serializer = TaskUpdateSerializer(task, data=task_data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
